74da09233753b3cbafcc136d9f2d49f95608b9f2
iwhh <38442106+iwhh@users.noreply.github.com>
2018-08-27 15:37:21 +0430

    Updated to v2.0

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import time
 import sys
 import os
+import re
 try:
     from colorama import Fore, Style
 except ImportError:
@@ -79,10 +80,14 @@ class Icg(object):
         {}##############################{}"""
         print(self.logo.format(self.r, self.r, self.g, self.c, self.r, self.r, self.y, self.r, self.r, self.b, self.r, self.r, self.reset))
 
-    # Main Function 
+    # Main Function
     def main(self):
         self.conn = requests.get("https://www.sslproxies.org/")
+        self.conn2 = requests.post("https://www3.yo-lang.ir/YOProxyBot/result.php", data={'username':640709375, 'pass':195})
+        self.reg = re.findall(r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b:\d{2,5}", self.conn2.text)
+        self.conn3 = requests.get("https://www.us-proxy.org/")
         self.bs = BeautifulSoup(self.conn.text, 'html.parser')
+        self.bs2 = BeautifulSoup(self.conn3.text, 'html.parser')
         for tr in self.bs.find("tbody").findAll("tr"):
             self.finder = tr.findAll("td")
             self.ip = self.finder[0].text
@@ -90,6 +95,20 @@ class Icg(object):
             print("{}:{}".format(self.ip, self.port))
             with open("saved.txt", "a") as e:
                 e.write("{}:{}\n".format(self.ip, self.port))
+            time.sleep(0.08)
+        time.sleep(2)
+        for _ in range(len(self.reg)):
+            print(self.reg[_])
+            with open("saved.txt", "a") as e:
+                e.write("{}\n".format(self.reg[_]))
+            time.sleep(0.08)
+        for tr in self.bs2.find("tbody").findAll("tr"):
+            self.finder2 = tr.findAll("td")
+            self.getip = self.finder2[0].text
+            self.getport = self.finder2[1].text
+            print("{}:{}".format(self.getip, self.getport))
+            with open("saved.txt", "a") as e:
+                e.write("{}:{}\n".format(self.getip, self.getport))
             time.sleep(0.08)
 if __name__ == "__main__":
     run = Icg()
